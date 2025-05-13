f3dcc1931969abf3d4330d7a8a1cd08ef3e3cd08
BRUNO DUILIO MASSACCESI <48590334@ort>
2025-05-13 09:03:11 -0300

    muchos avanzes del pressable, buttons. touchableopacity, estilos y de mas

--- tp7_Componentes_en_rn/App.js
+++ tp7_Componentes_en_rn/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, TouchableOpacity, Pressable, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, TouchableOpacity, Pressable, Image, Button } from 'react-native';
+import React, { useState } from 'react';
 
 const imgLocal = require('./assets/Fondo1.avif');
-const imgLocal2 = require('./assets/fondo.jpg');
+const imgLocal2 = require('./assets/fondo.jpeg');
+
 
 export default function App() {
+const [contador, setContador] = useState(0);
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
@@ -18,7 +22,29 @@ export default function App() {
               source={imgLocal2}
               style={styles.foto}
             />
-            <Text style={styles.nombre}>Tu Nombre</Text>
+            <Text style={styles.nombre}>DreamMatch</Text>
+            <Text style={styles.textContador}>Contador de VIRGOS:</Text>
+
+            <TouchableOpacity style={styles.boton}>  
+                  <Text style={styles.numeroContador}>{contador}</Text>
+            </TouchableOpacity>
+            <Pressable
+                style={styles.boton}
+                onPress={() => setContador(contador + 1) }>
+                <Text style={styles.textoBoton}>Soy un Virgo</Text>
+            </Pressable>
+            <TextInput
+            style={styles.input}
+            placeholder='¿No sos un virgo? , justifica aca...'
+            />
+            <TouchableOpacity style={styles.justificar} onPress={() => alert('No te cree nadie GOVIRRRR')}>
+              <Text style={styles.just}>justificar</Text>
+            </TouchableOpacity>
+
+
+            
+
+            
        
         </ImageBackground>
       </View>
@@ -36,8 +62,8 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center',     
+    alignItems: 'center',
+    paddingTop:30,     
   },
   profileContainer: {
     backgroundColor: 'rgba(255,255,255,0.8)',
@@ -45,13 +71,60 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   foto: {
-    width: 110,
+    width: 170,
     height: 140,
     borderRadius: 60,
-    marginBottom: 30,
+    marginBottom: 10,
   },
   nombre: {
     fontSize: 18,
     fontWeight: 'bold',
+    fontSize:30,
+  },
+  textContador:
+  {
+    fontSize:30,
+    marginTop:10,
+    fontWeight: 'bold',
+    color:'grey',
+  },
+  boton:
+  {
+    backgroundColor: '#fff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  textoBoton:
+  {
+    fontWeight:'bold',
+  },
+  pregunta:
+  {
+    fontWeight:'bold',
+    marginTop:30,
+  },
+  input:
+  {
+    height: 40,
+    width: '70%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 10,
+    backgroundColor: 'lightGrey',
+    color: '#000',
+    textAlign: 'center',
+
+  },
+  just:
+  {
+    backgroundColor: 'grey',
+    fontWeight:'bold',
+    marginTop:10,
+    padding:5,
+    borderRadius:5,
+
   },
 });
